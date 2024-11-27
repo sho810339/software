eba7ff34e176ab2543d30456a138161ec881fe0b
sho810339 <118622251+sho810339@users.noreply.github.com>
2024-11-27 23:25:52 +0800

    modify after change workerModel

--- controllers/workerController.js
+++ controllers/workerController.js
@@ -1,10 +1,10 @@
-const Worker = require('../models/Worker');
+const Worker = require('../models/worker');
 
 // 新增船員
 const addWorker = async (req, res) => {
-  const { worker_id, given_name, family_name, age, country, passport_number, job_title } = req.body;
+  const { given_name, family_name, age, country, passport_number, job_title } = req.body;
   try {
-    const newWorker = await Worker.create({ worker_id, given_name, family_name, age, country, passport_number, job_title });
+    const newWorker = await Worker.create({ given_name, family_name, age, country, passport_number, job_title });
     res.json({ message: '新增船員成功', newWorker });
   } catch (error) {
     console.error('新增船員失敗:', error.errors || error.message); // 顯示具體錯誤
@@ -14,9 +14,9 @@ const addWorker = async (req, res) => {
 
 // 查詢單一船員
 const getWorker = async (req, res) => {
-  const { id } = req.params;
+  const { worker_id } = req.params;
   try {
-    const worker = await Worker.findByPk(id);
+    const worker = await Worker.findByPk(worker_id);
     if (!worker) return res.status(404).json({ error: '找不到該船員' });
     res.json(worker);
   } catch (error) {

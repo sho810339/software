e401b9f9ce93ed90b70c6fe0af3678c1e80a4472
Xavi3r4 <134699927+Xavi3r4@users.noreply.github.com>
2024-12-07 15:54:32 +0800

    Delete test directory

--- test/config/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-// 設置 SQLite 資料庫
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite',  // SQLite 資料庫檔案
-});
-
-module.exports = sequelize;

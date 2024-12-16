66dca960c907523a5793aa2070e6940debf13f3f
bernie0224 <90887761+bernie0224@users.noreply.github.com>
2024-12-16 17:22:11 +0800

    新增關聯

--- models/index.js
+++ models/index.js
@@ -5,4 +5,7 @@ const user_login = require('./user_login');
 const report = require('./report');
 // 將模型集中導出
 
+crew_members.hasOne(user_login, { foreignKey: 'worker_id', sourceKey: 'worker_id' });
+user_login.belongsTo(crew_members, { foreignKey: 'worker_id', targetKey: 'worker_id' });
+
 module.exports = { work_hours, crew_members, notification, user_login, report };
